a1e7c8e7211178837774f0988cd94437fc335881
poojithamaru9 <poojithamaru9@gmail.com>
2023-02-07 14:53:52 -0500

    Added parcel and running on server

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
+/**
+ * Parcel do all things below(All are transitive dependecies)
+ * 
+ * Created server
+ * HRM- Hot MOdule Replacement
+ * File watcher algorithm-- (written in C++)
+ * BUNDLING
+ * MINIFY
+ * Cleaning our code
+ * Dev and Production build
+ * Super Fast build algorithm
+ * Image optimization
+ * Caching while developement
+ * Compression
+ * Compatable with older version of browser
+ * HTTPS on dev (npm parcel index.html --https) to run on https in local but localhost will not trust by chrome browser.
+ * Port Number
+ * Consistant Hashing algorithm
+ * Zero config 
+ * 
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const h1Element = React.createElement(
     "h1",
     {
         id:"heading"
     },
-    "Sample React App"
+    "Sample React App by Pujitha Maru"
 )
 
 const container = React.createElement(
